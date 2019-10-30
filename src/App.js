59eb9f9a486f219092a4f0e706903e3002432c98
Alan Krajina <alankrajina88@gmail.com>
2019-10-30 17:24:20 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { connect } from 'react-redux';                 // imported connect
 import './App.css';
 
 
- 
+
 class App extends Component {
   handleOnClick = event => {
-    this.props.increaseCount();  // increaseCount is actually being provided by the new function below our App 
+    this.props.increaseCount();  // increaseCount is actually being provided by the new function below our App
                                  //                                              component: mapDispatchToProps.
-  
+
   };
- 
+
   render() {
     return (
       <div className="App">
-        <button onClick={this.handleOnClick}>Click</button>                                            
-        <p>{this.props.items.length}</p>  // prop created from our Redux store. 
+        <button onClick={this.handleOnClick}>Click</button>
+        <p>{this.props.items.length}</p>  // prop created from our Redux store.
                                  // As the store's items property increases, App will display a different number!
       </div>         /
     );
@@ -24,20 +24,20 @@ class App extends Component {
 
 
 
- 
+
 const mapStateToProps = state => {
   return {
     items: state.items
   };
 };
- 
+
 const mapDispatchToProps = dispatch => {
   return {
     increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
   };
 };
- 
-export default connect(        
+
+export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(App);
