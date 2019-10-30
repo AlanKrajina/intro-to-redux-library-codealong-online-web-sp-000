4befcf9ae27405ec575893e12926b9ef973834bd
Alan Krajina <alankrajina88@gmail.com>
2019-10-30 17:25:01 +0000

    Automatically backed up by Learn

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -10,7 +10,7 @@ export default function shoppingListItemReducer(     // has a STATE and ACTION a
         ...state,
         items: state.items.concat(state.items.length + 1)
       }   // adds a new item to the list each time it is called, and that item is one more than the last item.
- 
+
     default:
       return state;
   }
